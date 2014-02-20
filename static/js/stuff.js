dfa1f69f427d523b5056a53ffc52bb85f78c78ad
MARIANA <msosacordero@berkeley.edu>
2014-02-19 20:29:20 -0800

    ‘

--- static/js/stuff.js
+++ static/js/stuff.js
@@ -1,25 +1,41 @@
 alert('Wilkommen zu meine Seite!'); // edit me!
 
 // Problem 1 (Say Hello!) ---------------------------------------------------
-$('#say_hello').click(function() {
-	alert('hello world')
-  // WRITE CODE HERE
+$('#say_hello').click(function() 
+{
+	alert('hello world!');
 });
 
 
 // Problem 2 (Houdini) ------------------------------------------------------
-$('#disappear').click(function() {
-  //WRITE CODE HERE
+$('#disappear').click(function() 
+{
+	$(houdini_text).hide();
 });
 
 $('#reappear').click(function() {
-  //WRITE CODE HERE
+	$(houdini_text).show();
 });
 
 
 // Problem 3 (Tickle Me Pink) -----------------------------------------------
-// WRITE CODE HERE
+$('#tickle_me').click(function()
+{
+	$(tickle_text).css('color','pink');
+
+});// WRITE CODE HERE
 
 
 // Problem 4 (Greet Me) -----------------------------------------------------
 // WRITE CODE HERE
+$('#greet').click(function() 
+{
+	alert($('#my_name').val())
+
+});
+
+
+
+
+
+
