a1addf4187d516f5563d57a67a7d30b5b016e335
MARIANA <msosacordero@berkeley.edu>
2014-03-02 12:48:24 -0800

    ‘

--- static/js/imgrr.js
+++ static/js/imgrr.js
@@ -1,13 +1,35 @@
 // Problem 2 (Peekaboo) ------------------------------------------------------
 // WRITE CODE HERE
+$('#toggle_img').click(function() 
+{
+	if ($('#toggle_img').text()==='Go Away!'){
+		$('#main_img').hide();
+		$('#toggle_img').text('Come Back!');
+	}	
+	else{
+	$('#main_img').show();
+	$('#toggle_img').text('Go Away!');
+	}
+});
 
 // Problem 3 (Swap Em) -----------------------------------------------
 // WRITE CODE HERE
+$('#change_img').click(function() 
+{
+	alert($('#new_img_file').val());
+});
+
+
 
 // Problem 4 (Find the Source) -------------------------------------------------
 $('.clickable').click(function() {
   // WRITE CODE HERE
+
+  
 });
+
+
+
 
 // Problem 5 (Imgrr) -------------------------------------------------
 // WRITE CODE HERE
