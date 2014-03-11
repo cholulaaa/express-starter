e2b36469b9964b3b7a12df8291da49c4851ad8bf
MARIANA <msosacordero@berkeley.edu>
2014-03-10 19:42:54 -0700

    ‘

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -21,7 +21,39 @@ $(document).ready(function() {
   	var c=document.getElementById('canvas');
 	var ctx=c.getContext('2d');
 
-  	context.strokeRect(150, 150, 50, 50);
+  	context.strokeRect(200, 200, 50, 50);
   });
+
+  $('#p3').click(function() {
+  	var c=document.getElementById('canvas');
+  	var ctx=c.getContext('2d');
+
+  	context.beginPath();
+  	context.arc(50,450,50,0*Math.PI,1.5*Math.PI);
+  	ctx.stroke();
+
+  });
+
+  $('#p4').click(function() {
+  	var c=document.getElementById('canvas');
+  	var ctx=c.getContext('2d');
+
+  	context.beginPath();
+
+  	ctx.arc(400,400,50,0,2*Math.PI);
+	ctx.stroke();
+});
+
+$('#p5').click(function() {
+  	var c=document.getElementById('canvas');
+  	var ctx=c.getContext('2d');
+
+  	context.beginPath();
+  	context.moveTo(0, 0);
+  	context.lineTo(300, 150);
+  	ctx.stroke();
+});
+
+
 
 });
