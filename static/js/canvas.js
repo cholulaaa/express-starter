f5e1d4cfa34b1c37435659d8f0c4618bf86ffb40
MARIANA <msosacordero@berkeley.edu>
2014-03-10 22:24:23 -0700

    I DID IT! for loops!

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -51,8 +51,66 @@ $('#p5').click(function() {
   	context.beginPath();
   	context.moveTo(0, 0);
   	context.lineTo(300, 150);
+  	context.closePath(); 
   	ctx.stroke();
 });
+
+$('#p6').click(function() {
+  	var c=document.getElementById('canvas');
+	var ctx=c.getContext('2d');
+
+  	ctx.fillStyle="green";
+  	context.fillRect(250, 50, 150, 100);
+  	
+  });
+
+  $('#p7').click(function() {
+  	var c=document.getElementById('canvas');
+  	var ctx=c.getContext('2d');
+
+  	context.beginPath();
+
+  	ctx.fillStyle="red";
+  	ctx.arc(200,400,50,0,2*Math.PI);
+	ctx.fill();
+});
+
+$('#p8').click(function() {
+  	var c=document.getElementById('canvas');
+	var ctx=c.getContext('2d');
+
+ 	context.beginPath();
+      context.rect(100, 245, 100, 100);
+      context.fillStyle = 'yellow';
+      context.fill();
+      context.lineWidth = 4;
+      context.strokeStyle = 'blue';
+      context.stroke();
+    context.closePath(); 
+  
+  });
+
+
+$('#p9').click(function() {
+  	var c=document.getElementById('canvas');
+	var ctx=c.getContext('2d');
+
+	for (var i = 1; i <= 6; i++) {
+		context.strokeRect(50*i, 100, 50, 50);
+    }
+
+});
+
+$('#p10').click(function() {
+  	var c=document.getElementById('canvas');
+	var ctx=c.getContext('2d');
+
+	for (var i = 1; i <= 101; i++) {
+		context.strokeRect(5*i, 100, 5, 5);
+    }
+
+});
+
 
 
 
