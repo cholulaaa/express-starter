2213335d827d64f8aa4fd444a5f01eb6edc6745e
MARIANA <msosacordero@berkeley.edu>
2014-04-02 20:43:13 -0700

    the balls move!

--- static/js/chain_reaction.js
+++ static/js/chain_reaction.js
@@ -10,17 +10,23 @@ var balls = [];
   var b0 = {
     x: 20,
     y: 20,
-    radius: 20
+    radius: 20,
+    vx: 10,
+    vy: 10
   };
   var b1 = {
     x: 140,
     y: 40,
-    radius: 40
+    radius: 40,
+    vx: 5,
+    vy: 5
   };
   var b2 = {
     x: 255,
     y: 155,
-    radius: 35
+    radius: 35,
+    vx: 10,
+    vy: 5
   };
   balls.push(b0);
   balls.push(b1);
@@ -28,13 +34,32 @@ var balls = [];
   //alert(balls.length)
   
  var updateGame = function() {
+  for(var i = 0; i < balls.length; i++){
+    balls[i].x = balls[i].x + balls[i].vx;
+    balls[i].y = balls[i].y + balls[i].vy;
+     if (balls[i].x == 0 + balls[i].radius){
+      balls[i].vx = -balls[i].vx;
+    }
+    if (balls[i].x == width - balls[i].radius){
+      balls[i].vx = -balls[i].vx;
+    }
+    if (balls[i].y == 0 + balls[i].radius){
+      balls[i].vy = -balls[i].vy;
+    }
+    if (balls[i].y == height - balls[i].radius){
+      balls[i].vy = -balls[i].vy;
+    }
+  }
+
   context.clearRect(0, 0, canvas.width, canvas.height);
   for(var i = 0; i < balls.length; i++){
     context.beginPath();
     context.strokeStyle = 'black';
     context.arc(balls[i].x, balls[i].y, balls[i].radius, 0, 2*Math.PI);
     context.stroke();
-  };
+  }
+
+  setTimeout(updateGame, 10);
 
   };
   
