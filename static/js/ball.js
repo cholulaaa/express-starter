70fd72afbf7b1cfbc45e270d22038d8033dfd2ba
MARIANA <msosacordero@berkeley.edu>
2014-04-02 17:39:16 -0700

    DID IT!

--- static/js/ball.js
+++ static/js/ball.js
@@ -8,15 +8,40 @@ $(document).ready(function() {
   //PUT STUFF HERE
 
   //run an iteration of the game
-  var x = 20
+
+
+  var ball = {
+    x: 20,
+    y: 20,
+    radius: 20,
+    vx: 5,
+    vy: 5
+
+  };
+
   var updateGame = function() {
-    x = x + 5;
+    ball.x = ball.x + ball.vx;
+    ball.y = ball.y + ball.vy;
+    context.clearRect(0, 0, canvas.width, canvas.height);
     context.beginPath();
     context.strokeStyle = 'black';
-    context.arc(x, 20, 20, 0, 2*Math.PI);
+    context.arc(ball.x, ball.y, ball.radius, 0, 2*Math.PI);
     context.stroke();
+    if (ball.x == 0 + ball.radius){
+      ball.vx = -ball.vx;
+    }
+    if (ball.x == width - ball.radius){
+      ball.vx = -ball.vx;
+    }
+    if (ball.y == 0 + ball.radius){
+      ball.vy = -ball.vy;
+    }
+    if (ball.y == height - ball.radius){
+      ball.vy = -ball.vy;
+    }
+
     setTimeout(updateGame, 10);
-    //PUT STUFF HERE
+    
   };
 
   updateGame();
